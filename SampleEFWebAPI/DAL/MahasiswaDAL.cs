38a91d63adb4ca76afa038175c87cd9bc987065a
erick <erick@DESKTOP-7DP9Q9N>
2016-10-12 10:07:46 +0700

    menambahkan mahasiswaDAL

---
+++ SampleEFWebAPI/DAL/MahasiswaDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using SampleEFWebAPI.Models;
+
+namespace SampleEFWebAPI.DAL
+{
+    public class MahasiswaDAL
+    {
+        private SampleKSIDbEntities db;
+
+        public MahasiswaDAL()
+        {
+            db = new SampleKSIDbEntities();
+        }
+    }
+}
